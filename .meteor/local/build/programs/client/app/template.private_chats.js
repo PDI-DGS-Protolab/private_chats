8e994cb2a9873a62d5d66d88fbd238f505116f87
developer <jonatanfmembrives@gmail.com>
2013-12-02 11:40:01 +0100

    adding meteor files

---
+++ .meteor/local/build/programs/client/app/template.private_chats.js
@@ -0,0 +1,3 @@
+(function(){Meteor.startup(function(){document.body.appendChild(Spark.render(Template.__define__(null,Package.handlebars.Handlebars.json_ast_to_func(["<h1>Private Chats!</h1>"]))));});
+
+})();
