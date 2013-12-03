78d316d8e80172d3fe2d515654c876d01e0a17a6
coboshm <coboshernandez@gmail.com>
2013-12-03 13:22:32 +0100

    server version1

--- server.js
+++ server.js
@@ -1,15 +1,56 @@
+Messages = new Meteor.Collection('messages');
+
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to server.";
+
+  Meteor.Router.add({
+  '/': 'messagesList',
+});
+
+  Template.messagesList.helpers({
+      messagesList: function() { return Messages.find({},{sort :{time: 1}}); }
+  });
+
+  Template.messagesList.messages = function(){
+    return Messages.find({},{sort : {time: 1}});
   };
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+  Template.messagesList.backButton=function(){
+      return localStorage.serverBack;
+  };
+
+
+
+  Template.messagesList.events({
+      'keydown input#newtext' : function(event){
+      if(event.which == 13){ //13 == Enter key event
+        var newName= localStorage.name;
+        var newText= document.getElementById('newtext');
+        if(newName.value != '' && newText.value != ''){
+          Messages.insert({
+            author: newName,
+            text: newText.value,
+            time: Date.now(),
+          });
+          document.getElementsByTagName('newtext').value = '';
+          newText.value = '';
+        }
+      }
+    },
+    'click button#newmessage': function () {
+        var newName= localStorage.name;
+        var newText= document.getElementById('newtext');
+        if(newName.value != '' && newText.value != ''){
+          Messages.insert({
+            author: newName,
+            text: newText.value,
+            time: Date.now(),
+          }); 
+          document.getElementsByTagName('newtext').value = '';
+          newText.value = '';
+      }
     }
   });
+
 }
 
 if (Meteor.isServer) {
