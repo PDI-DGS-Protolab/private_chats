8f59bda17433974b11a0d09bf4c08c4907dd8753
developer <boschpons@gmail.com>
2013-12-05 08:54:20 +0100

    Fixed bottomBar at the Bottom and some css changes

--- homepage.js
+++ homepage.js
@@ -1,11 +1,5 @@
 if (Meteor.isClient) {
 
-  // $(document).ready(function () {
-  //   if (localStorage.name != null) {
-  //     document.location.href = document.location.href + 'rooms';
-  //   };
-  // });
-
   Template.homepage.rendered = function () {
     if (localStorage.name != null) {     
       Router.go('rooms');
