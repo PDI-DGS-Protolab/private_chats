29d0612396eabe28a6ecd3d10411d141a5bfffd1
Jaume <boschpons@gmail.com>
2013-12-19 14:54:30 +0100

    Deleted comented old code

--- src/roomspage/rooms.js
+++ src/roomspage/rooms.js
@@ -47,40 +47,6 @@ if (Meteor.isClient) {
       'click div.divTextRoom' : function(e) {
         goToUrl(e);
       },
-/*      'click a.urlRoom': function (e) {
-        //resolveUrl(e);
-        //TODO : properly assign user token authRoom parameters
-
-        var roomIdChat =  getSegment(e.currentTarget.id,2);
-        var roomURL =  e.currentTarget.id;
-        var user =localStorage.getItem("Meteor.userId");//change to tokens
-
-        var roomAuth ='roomAuth';//change to tokens
-
-        Meteor.call('getRoomIdAuth', roomURL, roomIdChat, function (err,data){
-
-          if(err){
-            console.log(err);}
-            else{
-              Session.set('q',data);
-
-             // console.log(data);
-            }
-         });
-
-       console.log(Session.get('q'));
-        var res=Session.get('q');
-        //TODO : get _id from res and pass everything to chatServ (uncomment 68,69)
-        console.log(res["_id"]);
-        var roomAuth=res._id;
-        //check if is working with IE
-        //var url = e.currentTarget.id + '?user=' + user +'?roomAuth='+roomAuth;
-       // window.location.href = "http://"+url;
-
-      },
-      'click a.nameRoom': function (e) {
-        resolveUrl(e);
-      },*/
       'click button.delRoom':function(e){
         var idRoom = e.currentTarget.id;
 
