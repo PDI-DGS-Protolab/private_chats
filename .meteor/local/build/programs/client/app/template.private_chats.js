36008d58c63240994ca0b296247cd22f91bc80fb
Marc Cobos <coboshernandez@gmail.com>
2014-01-07 12:10:22 +0100

    More fixes

--- .meteor/local/build/programs/client/app/template.private_chats.js
+++ .meteor/local/build/programs/client/app/template.private_chats.js
@@ -1,3 +1,1 @@
-(function(){Meteor.startup(function(){document.body.appendChild(Spark.render(Template.__define__(null,Package.handlebars.Handlebars.json_ast_to_func(["<h1>Private Chats!</h1>"]))));});
-
-})();
+hi
