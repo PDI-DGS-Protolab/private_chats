0c0eee9aafc185f1562171b4b5a084355a930a46
marccomino <marccomino@gmail.com>
2014-01-21 09:05:56 +0100

    Commenting the Dockerfile

--- DockerSetup/execute.sh
+++ DockerSetup/execute.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+
 cd auth_server
+
+# Executing meteor.
 mrt
--- DockerSetup/install_auth_server.sh
+++ DockerSetup/install_auth_server.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Descargar el código del repositorio de github.
+# Download the code from the Github repository.
 git clone https://github.com/PDI-DGS-Protolab/private_chats.git auth_server
 cd auth_server
 git checkout master_authServer
 git pull
 
-# Actualizar las dependencias del proyecto
+# Updating the project dependencies.
 mrt add iron-router
 mrt add http
 mrt add accounts-password
 
 cd
 
-#Quickfix
+# Fixing the source code of meteor in order to avoid crashes.
 sed -i '452s/.*/files.rm_recursive(toDir);/' /.meteor/tools/09b63f1ed5/tools/files.js
 sed -i '450s/.*/var movedOldDir = false;/' /.meteor/tools/09b63f1ed5/tools/files.js
